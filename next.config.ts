6ac6ec25f77efba0a938bab732b25fc1593aae9b
Serdar Akgul <serdar@expiya.com>
2026-08-17 14:03:07 +0300

    fix(cars): include persona release in server trace

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
+  outputFileTracingIncludes: {
+    "/api/cars/conversation": ["./data/production/personas/safe-traits/**/*"],
+  },
   images: {
     remotePatterns: [{
       protocol: "https",
